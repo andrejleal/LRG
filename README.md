9017ee0bfc4cc228bb6eee8b77764947266ce08a
André Leal <andrejleal@gmail.com>
2014-11-18 11:01:26 +0000

    Added LRG to github.

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-LaravelResourceGenerator
+LRG - Node.js Laravel Resource Generator From Excel (XLSX)
 ========================
 
-Resource generator for Laravel Applications
+LRG is an application that reads a excel file and generates a output directory with resource files for Laravel applications.
+
+###Prerequisites:
+
+Install the following node.js modules:
+  - node-xlsx
+```sh
+$ npm install node-xlsx
+```
+  
+  - config
+```sh
+$ npm install config
+```
+
+
+
+###Running LRG:
+```sh
+$ node lrg
+```
+
---
+++ lrg.js
@@ -0,0 +1,79 @@
+
+process.stdout.write("Start generating resources...\n");
+
+var xlsx = require('node-xlsx');
+var fs = require('fs');
+var config = require('config');
+
+
+if(!fs.existsSync(config.sourceFile)){
+	process.stdout.write('Source file does not exist.\n');
+}
+else {
+
+	var excelFile = xlsx.parse(config.sourceFile); // parses a file
+	var outputDir = config.outputDirectory;
+	var keyCellIndex = config.keyCellIndex;
+	var languageFirstCellIndex = config.languageFirstCellIndex;
+
+	if(!fs.existsSync(outputDir)){
+		process.stdout.write('Creating output directory.\n');
+		fs.mkdirSync(outputDir);
+	}
+
+	for(var sheetIdx = 0; sheetIdx < excelFile.length;sheetIdx++){
+		
+		var sheet = excelFile[sheetIdx];
+		
+		var availableLanguages = [];
+		for(var cellIdx = languageFirstCellIndex; cellIdx < sheet.data[0].length; ++cellIdx){
+			var lang = sheet.data[0][cellIdx];
+			availableLanguages.push(
+			 { 
+				idx : cellIdx,
+				language: lang
+			 }
+			);
+			process.stdout.write('Add language ['+lang+'] for current sheet\n');
+			
+			var langOutputDir = outputDir+lang;
+			if(!fs.existsSync(langOutputDir)){
+				fs.mkdirSync(langOutputDir);
+			}
+			
+		}
+		
+		for(var langIdx in availableLanguages){
+			process.stdout.write('process language ['+availableLanguages[langIdx].language+']\n');
+			
+			var filePath = outputDir + availableLanguages[langIdx].language + '/'+sheet.name + '.php';
+			var wstream = fs.createWriteStream(filePath);
+			wstream.write('<?php\n');
+			wstream.write('\n');
+			wstream.write('return array(\n');
+			
+			var langCellIdx = availableLanguages[langIdx].idx;
+			for(var row = 1; row < sheet.data.length; ++row){
+				var key = sheet.data[row][keyCellIndex];
+				var txt = sheet.data[row][langCellIdx];
+				if(txt && txt!= null)
+				{
+					wstream.write('			');
+					wstream.write('"'+key+'" =>');
+					wstream.write('"'+txt+'",\n');
+				}
+			}		
+			
+			wstream.write(');\n');
+			wstream.end();	
+		}
+		
+
+
+}
+}
+
+
+
+
+process.stdout.write("Execution completed...\n");
